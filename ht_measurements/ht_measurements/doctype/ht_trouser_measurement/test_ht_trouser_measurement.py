7651a9f6cf30644edc8b8755abc5ffa810eb1147
ananthuuu <ananthcjayan12@gmail.com>
2024-06-14 10:23:25 +0000

    added javascripts for the ht order submit form

---
+++ ht_measurements/ht_measurements/doctype/ht_trouser_measurement/test_ht_trouser_measurement.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ananthcjayan12 and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHt_Trouser_Measurement(FrappeTestCase):
+	pass
