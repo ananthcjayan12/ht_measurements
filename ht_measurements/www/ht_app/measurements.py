9464f4c487a8374e90179719de4ec50f16d06ac6
ananthcjayan12 <123536374+ananthcjayan12@users.noreply.github.com>
2024-06-27 15:48:47 +0530

    Br new branch ananth ramees stg (#6)
    * Added my changes
    
    * First design update from ht (#5)
    
    * done changes
    
    * added the scrip changes
    
    * added javascripts for the ht order submit form
    
    * renamed
    
    * Added jscodes for ordersubmit doctype (#3)
    
    * done
    
    * added all changes
    
    * Added my changes (#4)
    
    ---------
    
    Co-authored-by: ananthuuu <ananthcjayan12@gmail.com>
    Co-authored-by: ananthcjayan12 <123536374+ananthcjayan12@users.noreply.github.com>
    
    * added style sheet and and forms- initial
    
    * added changes
    
    ---------
    
    Co-authored-by: rramees <ramees8614@gmail.com>
    Co-authored-by: rramees <121540985+rramees@users.noreply.github.com>

--- ht_measurements/www/ht_app/measurements.py
+++ ht_measurements/www/ht_app/measurements.py
@@ -4,8 +4,20 @@ def get_context(context):
     invoice_number = frappe.form_dict.get('invoice_number')
     context.invoice_number = invoice_number
 
-    # Check if measurements are completed
-    context.shirt_completed = frappe.db.exists('Shirt Measurement', {'invoice': invoice_number, 'docstatus': 1})
-    context.trousers_completed = frappe.db.exists('Ht_Trouser_Measurement', {'invoice': invoice_number, 'docstatus': 1})
+    # Check if the invoice number exists in the HT Invoice doctype
+    invoice_exists = frappe.db.exists('HT Invoice', {'invoice_number': invoice_number})
+    print(" I am here")
+    print(invoice_exists)
+
+    if not invoice_exists:
+        context.no_invoice_message = f'No invoice present with number {invoice_number}'
+        context.is_invoice_exists=False
+        context.shirt_completed = False
+        context.trousers_completed = False
+    else:
+        # Check if measurements are completed
+        context.is_invoice_exists=True
+        context.shirt_completed = frappe.db.exists('Shirt Measurement', {'invoice': invoice_number, 'docstatus': 1})
+        context.trousers_completed = frappe.db.exists('Trouser Measurement', {'invoice': invoice_number, 'docstatus': 1})
 
     return context
